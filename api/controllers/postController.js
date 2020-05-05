3def3f86c173c117dee66cf8f94e9ed1fe4a998b
alanblee <alanbenlee12@gmail.com>
2020-05-05 14:59:55 -0700

    added POST route to create new post

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -2,15 +2,36 @@ const posts = require("../../data/db");
 
 //GET get all posts
 module.exports.getPosts = async (req, res) => {
-  const allPosts = await posts.find();
-  if (allPosts) {
+  try {
+    const allPosts = await posts.find();
     res.status(200).json(allPosts);
-  } else {
+  } catch (err) {
     res
       .status(500)
-      .json({ error: "The posts information could not be retrieved." });
+      .json({ error: "The posts information could not be retrieved.", err });
   }
 };
 
 //POST create new post
-module.exports.createPost = async (req, res) => {};
+module.exports.createPost = async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title && !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  } else {
+    try {
+      const newPostInfo = {
+        title,
+        contents,
+      };
+      const newPost = await posts.insert(newPostInfo);
+      res.status(201).json(newPost);
+    } catch (err) {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+        err,
+      });
+    }
+  }
+};
